c80a07b101934095223627c60d7ebc035f040c11
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-08-15 15:08:35 +0430

    complete the Contact us section and connect with web3forms

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import TrustBy from "./components/TrustBy";
 import Serivces from "./components/Serivces";
 import Ourwork from "./components/Ourwork";
 import Team from "./components/Team";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -12,12 +14,14 @@ const App = () => {
   );
   return (
     <div className="dark:bg-black  relative">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustBy />
       <Serivces />
       <Ourwork />
       <Team />
+      <ContactUs />
     </div>
   );
 };
